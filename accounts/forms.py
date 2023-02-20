adc271fa05a5d87bc8c676af7ae37858eb960005
sotaheavymetal21 <euro246seeker@gmail.com>
2023-02-20 21:18:27 +0900

    formsを修正

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='ユーザー名')
-    password = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    username = forms.CharField(label="ユーザー名")
+    password = forms.CharField(label="パスワード", widget=forms.PasswordInput)
