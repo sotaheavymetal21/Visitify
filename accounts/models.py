278db94ac8ae0f61b5b882d3d8bdc94bc50d77df
sotaheavymetal21 <euro246seeker@gmail.com>
2023-02-21 22:21:58 +0900

    プロフィールモデルの作成、abstractuserを継承しようとするもうまくいかず

--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,14 @@
 from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+# class Profile(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     website = models.URLField(blank=True)
+#     self_introduction = models.TextField(blank=True)
+#     free_text = models.TextField(null=True, blank=True)
+
+#     def __str__(self):
+#         return self.username
