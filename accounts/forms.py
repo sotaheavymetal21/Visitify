79a8084af78d884e56e8c9cefb5130f2bc43f720
sotaheavymetal21 <euro246seeker@gmail.com>
2023-03-05 00:59:06 +0900

    👍 last_name, first_nameをフォームに追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserCreateForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required.')
+    email = forms.EmailField(required=True, help_text='必須です。', label='メールアドレス')
+
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -14,7 +19,7 @@ class UserCreateForm(UserCreationForm):
         return username
 
     def clean_email(self):
-        email = self.cleaned_data['email']
+        email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already taken.")
         return email
