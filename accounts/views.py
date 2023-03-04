2209227b529f48cf2d5f074d6099c576cb8a57a4
sotaheavymetal21 <euro246seeker@gmail.com>
2023-03-04 20:37:26 +0900

    ✨ integrity erorrの解決

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 
 from .models import User
-from .forms import UserProfileForm, UserCreateForm
+from .forms import UserCreateForm, UserProfileForm
 
 
 def home(request):
@@ -23,13 +23,18 @@ class CustomLoginView(LoginView):
     success_url = reverse_lazy("profile")
 
 
+from django.contrib.auth.hashers import make_password
+
 def signup(request):
     if request.method == "POST":
         user_form = UserCreateForm(request.POST)
         profile_form = UserProfileForm(request.POST, request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             # Userモデルの登録
-            user = user_form.save()
+            user = user_form.save(commit=False)
+            # パスワードをハッシュ化する
+            user.password = make_password(user_form.cleaned_data.get('password1'))
+            user.save()
             # UserProfileモデルの登録
             profile = profile_form.save(commit=False)
             profile.user = user
@@ -41,24 +46,6 @@ def signup(request):
             if user is not None:
                 login(request, user)
                 return redirect('profile')
-
-
-def signup(request):
-    if request.method == 'POST':
-        user_form = UserCreateForm(request.POST)
-        profile_form = UserProfileForm(request.POST, request.FILES)
-        if user_form.is_valid() and profile_form.is_valid():
-            # Userモデルの登録
-            user = user_form.save()
-            # UserProfileモデルの登録
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            # ログイン処理
-            raw_password = user_form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('profile')
     else:
         user_form = UserCreateForm()
         profile_form = UserProfileForm()
@@ -67,9 +54,11 @@ def signup(request):
 
 
 
+
+
 @login_required
 def profile(request):
-    user_profile = User.objects.get(user=request.user)
+    user_profile = request.user
     context = {'user_profile': user_profile}
     return render(request, 'profile.html', context)
 
